d3561d3df408a1ab6f6262fe2a9c0e7ed2a138f5
maudevjr <mauriciojr.dev@gmail.com>
2021-01-22 17:37:04 -0300

    add new db

--- api/src/controllers/task.controller.js
+++ api/src/controllers/task.controller.js
@@ -1,4 +1,4 @@
-const db = require('../config/database');
+const db = require('../config/postgres');
 
 exports.openTask = async (req, res) => {
     const { taskData, taskName, status  } = req.body;
